d3d843a255f527cadda2ec68a95cb46abfe6da12
akkayy <techakayy@gmail.com>
2022-06-22 20:06:19 +1000

    Installed Pinegrow vite plugin

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
 import vuetify from 'vite-plugin-vuetify'
+import { liveDesigner } from '@pinegrow/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-		vue(),
-		vuetify({ autoImport: true }),
-],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+	plugins: [liveDesigner(), vue(), vuetify({ autoImport: true })],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url))
+		}
+	}
 })
