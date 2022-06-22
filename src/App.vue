5cdfb9cb46f654d1ac557266feb28ededa22e51a
akkayy <techakayy@gmail.com>
2022-06-22 20:44:16 +1000

    Added app-bar for Home & About routes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <v-app>
-    <v-main>
+    <v-app-bar extension-height="48" height="64" model-value>
+            <v-btn ripple to="/">Home</v-btn>
+            <v-btn ripple to="/about">About</v-btn>
+        </v-app-bar><v-main>
       <router-view/>
     </v-main>
   </v-app>
