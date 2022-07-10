757ea081dd2e4137daf02c0691250bf77ebc69e9
akkayy <techakayy@gmail.com>
2022-07-10 11:18:24 +1000

    Fix to vuetify aliases hack

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,17 +4,17 @@ import 'vuetify/styles'
 
 // Vuetify
 import { createVuetify } from 'vuetify'
-import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
+import { aliases, mdi as mdiSvg } from 'vuetify/iconsets/mdi-svg'
 
 export default createVuetify({
 	icons: {
 		defaultSet: 'class',
 		aliases: Object.keys(aliases).reduce((obj, key) => {
-        obj[key] = `${aliases[key]}`
-        return obj
-    }, {}),
+			obj[key] = `mdiSvg:${aliases[key]}`
+			return obj
+		}, {}),
 		sets: {
-			mdi
+			mdiSvg
 		}
 	}
 })
